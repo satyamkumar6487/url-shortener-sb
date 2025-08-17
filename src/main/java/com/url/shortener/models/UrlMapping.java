723ec54ed89158300082d665b0470dd8e5d102b3
satyamkumar6487 <jsatyamkumar597@gmail.com>
2025-08-17 18:10:46 +0530

    Updated entity classes and configurations

--- src/main/java/com/url/shortener/models/UrlMapping.java
+++ src/main/java/com/url/shortener/models/UrlMapping.java
@@ -1,2 +1,28 @@
-package com.url.shortener.models;public class UrlMapping {
+package com.url.shortener.models;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.List;
+@Entity
+@Data
+public class UrlMapping {
+
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String orignalUrl;
+    private String shorUrl;
+    private int clickCount;
+    private LocalDateTime createdDate;
+
+
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @OneToMany(mappedBy = "urlMapping")
+    private List<ClickEvent> clickEvents;
 }
--- src/main/java/com/url/shortener/models/User.java
+++ src/main/java/com/url/shortener/models/User.java
@@ -1,2 +1,23 @@
-package com.url.shortener.models;public class User {
+package com.url.shortener.models;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String email;
+    private String username;
+    private String password;
+    private String  role= "ROLE_USER";
+
+
+
+
 }
