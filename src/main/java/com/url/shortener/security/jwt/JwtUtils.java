8e7d42dc81a89c7d16ef85d3a9a2c9364c1ff5a0
satyamkumar6487 <jsatyamkumar597@gmail.com>
2025-08-18 20:32:34 +0530

    Updated pom.xml, JwtUtils, UserDetailsImpl, and application.properties

--- src/main/java/com/url/shortener/security/jwt/JwtUtils.java
+++ src/main/java/com/url/shortener/security/jwt/JwtUtils.java
@@ -1,2 +1,80 @@
-package com.url.shortener.security.jwt;public class JwtUtils {
+package com.url.shortener.security.jwt;
+
+import com.url.shortener.service.UserDetailsImpl;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Value;
+
+import javax.crypto.SecretKey;
+import java.security.Key;
+import java.util.Date;
+import java.util.stream.Collectors;
+
+public class JwtUtils {
+    //Authorization: Bearer <JWT_TOKEN>
+
+    @Value("${jwt.secret}")
+    private String jwtSecret;
+
+    @Value("${jwt.expiration}")
+    private int jwtExpirationMs;
+
+
+    public String getJwtFromHeader(HttpServletRequest request) {
+        String bearerToken = request.getHeader("Authorization");
+        if (bearerToken != null && bearerToken.startsWith("Bearer ")) {
+            return bearerToken.substring(7);
+        }
+        return null;
+    }
+
+    public String genrateToken(UserDetailsImpl userDetails) {
+        String username = userDetails.getUsername();
+        String roles = userDetails.getAuthorities().stream()
+                .map(authority -> authority.getAuthority())
+                .collect(Collectors.joining(" "));
+
+        return Jwts.builder()
+                .setSubject(username)
+                .claim("roles", roles)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date((new Date().getTime() + jwtExpirationMs)))
+                .signWith(key())
+
+                .compact();
+
+    }
+
+
+    public String getUserNameFromJwtToken(String token) {
+        Claims claims = Jwts.parserBuilder()
+                .setSigningKey(key())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+        return claims.getSubject();
+    }
+
+    private Key key() {
+        return Keys.hmacShaKeyFor(Decoders.BASE64.decode(jwtSecret));
+    }
+
+    public boolean validateToken(String authToken) {
+
+        try {
+            Jwts.parserBuilder()
+                    .setSigningKey(key())
+                    .build()
+                    .parseClaimsJws(authToken);
+            return true;
+        } catch (Exception e) {
+            System.out.println("Invalid JWT" + e.getMessage());
+            return false;
+        }
+
+
+    }
 }
